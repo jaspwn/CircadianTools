ff286b0fc687f15582a048b0ab00e3dd4f4de980
jaspwn <jasonasomers@gmail.com>
2016-08-19 13:38:00 +0100

    Added code to add header information containing experimental parameters

--- mkdayLight.R
+++ mkdayLight.R
@@ -1,10 +1,6 @@
-library(ggplot2)
-library(scales)
-library(reshape2)
-library(lubridate)
-library(data.table)
-library(dplyr)
-library(gridExtra)
+### Take environmental monitor binned data file and make dayLight file for matlab flytoolbox functions
+
+## choose filename and set working directory
 
 filename <- file.choose()
 setwd(dirname(filename))
@@ -15,8 +11,8 @@ data <- read.table(filename, stringsAsFactors = FALSE)
 
 ## date, start and stop time
 
-datestart <- as.POSIXct(strptime('2016-07-29 10:00:00', format = "%Y-%m-%d %H:%M:%S"))
-datestop <- as.POSIXct(strptime('2016-08-15 09:55:00', format = "%Y-%m-%d %H:%M:%S"))
+datestart <- as.POSIXct(strptime('2016-07-29 00:00:00', format = "%Y-%m-%d %H:%M:%S"))
+datestop <- as.POSIXct(strptime('2016-08-15 00:00:00', format = "%Y-%m-%d %H:%M:%S"))
 
 ## convert time in data file to POSIX datetime
 
@@ -43,10 +39,28 @@ make_datetime_vec <- function(x) {
   time <- format(timelist[x], '%Y%m%d %H%M')
 }
 
-
-#daylight <- cat(paste(sapply(1:length(timelist), make_datetime_vec), '  ', sapply(1:length(timelist), make_light_vec), '\n', sep = ''))
+## call make_light_vec and make datetime_vec functions to coerce data into correct format
 
 daylight <- paste(sapply(1:length(timelist), make_datetime_vec), '  ', sapply(1:length(timelist), make_light_vec), sep = '')
 
+## create file heading with monitor name, start date, number of bins, bin length (mins) and first time bin
 
-lapply(daylight, write, 'test', append = TRUE)
+monitor <- tools::file_path_sans_ext(basename(filename))
+heading <- paste(monitor, '   ', format(datestart, '%d %b %Y'), sep = '')
+len <- length(timelist)
+bin.length <- 5
+starttime <- format(timelist[1], '%H%M')
+
+## Create appropriate filename
+
+name.split <- strsplit(monitor, 'Ct')
+name.split <- name.split[[1]]
+out.filename <- paste(name.split[1], 'CtDAYLIGHT', sep = '')
+
+## Write all data to file
+
+write(heading, out.filename, append = FALSE)
+write(len, out.filename, append = TRUE)
+write(bin.length, out.filename, append = TRUE)
+write(starttime, out.filename, append = TRUE)
+lapply(daylight, write, out.filename, append = TRUE)
